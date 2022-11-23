0fb51677ee099591915ec0293412539bb1fa39a9
hredua <helamaredua@gmail.com>
2022-11-23 14:14:48 -0400

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.get("/", (req, res) => {
 });
 
 // Private Route
-app.get("/user/:id", cors(), checkToken, async (req, res) => {
+app.get("/user/:id", cors, checkToken, async (req, res) => {
   const id = req.params.id;
 
   // check if user exists
@@ -147,29 +147,5 @@ mongoose
     console.log("Conectou ao banco!");
   })
   .catch((err) => console.log(err));
-  app.use(cors())
+  app.use(cors)
   app.listen(port)
-
-  const allowCors = fn => async (req, res) => {
-    res.setHeader('Access-Control-Allow-Credentials', true)
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    // another common pattern
-    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-    res.setHeader(
-      'Access-Control-Allow-Headers',
-      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-    )
-    if (req.method === 'OPTIONS') {
-      res.status(200).end()
-      return
-    }
-    return await fn(req, res)
-  }
-  
-  const handler = (req, res) => {
-    const d = new Date()
-    res.end(d.toString())
-  }
-  
-  module.exports = allowCors(handler)
